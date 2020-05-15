8e49640aa11c2019e60a8df768f247594f878fbe
barbik07 <njikibikoip@gmail.com>
2020-05-15 14:05:57 -0500

    I added my R markdown homework file

---
+++ Do/titanic.R
@@ -0,0 +1,10 @@
+#\\Initial GitHub setup
+
+library(git2r)
+library(githubinstall)
+git config --global user.email "njikibikoip@gmail.com"
+git config --global user.name "barth"
+
+library(usethis)
+use_git_config(user.name = "barbik07", user.email = "njikibikoip@gmail.com")
+
